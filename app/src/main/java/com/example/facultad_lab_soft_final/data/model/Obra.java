8b82e707cb4562ac298ab75b096148248fa71e5b
pedroodb <dalbianco.pedro@gmail.com>
2019-12-11 15:58:06 -0300

    Intenta ordenar por fechas pero no funciona

--- app/src/main/java/com/example/facultad_lab_soft_final/data/model/Obra.java
+++ app/src/main/java/com/example/facultad_lab_soft_final/data/model/Obra.java
@@ -1,22 +1,39 @@
 package com.example.facultad_lab_soft_final.data.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Obra extends Actividad{
 
+    UbicacionObra ubicacion;
     String elenco;
     String autor;
     String direccion;
     String sinopsis;
     String tematica;
-    FechaHora[] fechas;
+    ArrayList<FechaHora> fecha_hora;
 
-    public Obra(int id, String nombre, String descripcion, UbicacionObra ubicacion, String elenco, String autor, String direccion, String sinopsis, String tematica, FechaHora[] fechas) {
-        super(id, nombre, descripcion, ubicacion);
+    public Obra(int id, String nombre, String descripcion, UbicacionObra ubicacion, String elenco, String autor, String direccion, String sinopsis, String tematica, ArrayList<FechaHora> fecha_hora) {
+        super(id, nombre, descripcion);
+        this.ubicacion = ubicacion;
         this.elenco = elenco;
         this.autor = autor;
         this.direccion = direccion;
         this.sinopsis = sinopsis;
         this.tematica = tematica;
-        this.fechas = fechas;
+        this.fecha_hora = fecha_hora;
+    }
+
+
+    public UbicacionObra getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(UbicacionObra ubicacion) {
+        this.ubicacion = ubicacion;
     }
 
     public String getElenco() {
@@ -59,11 +76,37 @@ public class Obra extends Actividad{
         this.tematica = tematica;
     }
 
-    public FechaHora[] getFechas() {
-        return fechas;
+    public ArrayList<FechaHora> getFecha_hora() {
+        return fecha_hora;
     }
 
-    public void setFechas(FechaHora[] fechas) {
-        this.fechas = fechas;
+    public void setFecha_hora(ArrayList<FechaHora> fecha_hora) {
+        this.fecha_hora = fecha_hora;
+    }
+
+    public String getHora() {
+        return this.getFecha_hora().get(0).getHora();
+    }
+
+    public Date getDateTime() {
+        return this.getFecha_hora().get(0).getDateTime();
+    }
+
+    @Override
+    public void listarPorFecha(Map<Date, SortedSet<Actividad>> actividades) {
+        for (FechaHora fecha_hora : this.fecha_hora) {
+
+            ArrayList<FechaHora> fechaUnica = new ArrayList<FechaHora>();
+            fechaUnica.add(fecha_hora);
+            Obra obra = new Obra(this.id,this.nombre,this.autor,(UbicacionObra) this.ubicacion,this.elenco,this.autor,this.direccion,this.sinopsis,this.tematica,fechaUnica);
+
+            if (actividades.containsKey(fecha_hora.getDate())) {
+                actividades.get(fecha_hora.getDate()).add(obra);
+            } else {
+                SortedSet<Actividad> lista = new TreeSet<Actividad>();
+                lista.add(obra);
+                actividades.put(fecha_hora.getDate(),lista);
+            }
+        }
     }
 }
