42aa955e4ed626fd554c9f5775cede336d2689f6
pedroodb <dalbianco.pedro@gmail.com>
2019-12-11 00:07:21 -0300

    Tenemos estrella de notificaciones

--- app/src/main/java/com/example/facultad_lab_soft_final/MainActivity.java
+++ app/src/main/java/com/example/facultad_lab_soft_final/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.facultad_lab_soft_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+            case R.id.menu_item:   //this item has your app icon
+                Toast.makeText(this,"Tapped on icon",Toast.LENGTH_SHORT).show();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }}
